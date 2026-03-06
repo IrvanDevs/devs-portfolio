05d29130f333be2bb3be51d1947aebca7333575d
vandevs <irvangt08@gmail.com>
2026-03-06 22:26:10 +0700

    cv added

--- src/data/HireMeData.jsx
+++ src/data/HireMeData.jsx
@@ -1,5 +1,8 @@
+import cv from '../assets/cv.pdf'
+
 export const hireMe ={
     title: 'OPEN FOR FRONTEND OPPORTUNITIES.',
     desc: 'I’m currently seeking frontend developer roles where I can contribute to building structured, scalable, and user focused website.',
     whatsApp: 'https://wa.me/6281360802626',
+    cvLink: cv,
 }
